abca277197a17d3d6ab550ee35e910312c696a0d
EC2 Default User <ec2-user@ip-172-31-0-63.ec2.internal>
2022-10-30 14:05:14 +0000

    [Finish]4-10

--- db/migrate/20221030111343_create_post_images.rb
+++ db/migrate/20221030111343_create_post_images.rb
@@ -1,7 +1,13 @@
 class CreatePostImages < ActiveRecord::Migration[6.1]
   def change
     create_table :post_images do |t|
-
+      t.string :shop_name
+      t.text :caption
+      t.integer :user_id
+      # マイグレーションファイルの記述場所を間違えたため、
+      # ActiveModel::UnknownAttributeError　が起きた。
+      # rails db:rollback　して、rails db:migrate:status　で、down表記確認後、
+      # rails db:migrate　で解決
       t.timestamps
     end
   end
