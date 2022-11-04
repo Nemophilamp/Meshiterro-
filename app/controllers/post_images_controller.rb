d87c3d4a13d3ac8f8e7248861b5ddfa588cc97ce
EC2 Default User <ec2-user@ip-172-31-0-63.ec2.internal>
2022-11-04 05:39:15 +0000

    [Add]PostImagesコントローラー修正

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -7,6 +7,7 @@ class PostImagesController < ApplicationController
   def create
     @post_image = PostImage.new(post_image_params)
     @post_image.user_id = current_user.id
+    
     if @post_image.save
       redirect_to post_images_path
     else
@@ -28,7 +29,7 @@ class PostImagesController < ApplicationController
   def destroy
     @post_image =  PostImage.find(params[:id])
     @post_image.destroy
-    redirect_to PostImages_path
+    redirect_to Post_images_path
   end
 
   # 投稿データのストロングパラメータ
